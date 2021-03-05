54a1459e2efb1077f09e669e349762ac30b071be
olcayusta <olcayusta02@gmail.com>
2021-03-05 23:41:50 +0300

    another fix

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ app.get('/db', (req, res) => {
 
     client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
         if (err) throw err;
-        for (let row of res.rows) {
-            res.json(JSON.stringify(row))
-        }
+        res.json(res.rows)
         client.end();
     });
 })
