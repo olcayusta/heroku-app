dabbeb4c20b23a8b8663999c90376859f1143981
olcayusta <olcayusta02@gmail.com>
2021-03-05 21:48:53 +0300

    make it better

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Node.js uygulaması ${PORT} üzerinden ayağa kaldırıldı.`)
+})
