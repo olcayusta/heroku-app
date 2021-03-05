b2e0bd7d256c2b862f7f265962f6116f3a4e1bf2
olcayusta <olcayusta02@gmail.com>
2021-03-05 23:32:24 +0300

    added pg

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import express from 'express'
+import {Client} from 'pg'
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
 const app = express()
 
@@ -8,6 +16,18 @@ app.get('/', (req, res) => {
 
 app.get('/port', (req, res) => {
     res.send(process.env.PORT)
+})
+
+app.get('/db', (req, res) => {
+    client.connect()
+
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
 })
 
 const PORT = process.env.PORT || 3000
